a10962cba2def96ca736341838172a9d9ba8681b
thecybercv <thecybercv@gmail.com>
2025-09-05 21:35:34 +0530

    Initial portfolio commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cyberorange: '#ff8800',
+        cyberblack: '#18181b',
+      },
+    },
+  },
+  plugins: [],
+};
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cyberorange: '#ff8800',
+        cyberblack: '#18181b',
+      },
+    },
+  },
+  plugins: [],
+};
